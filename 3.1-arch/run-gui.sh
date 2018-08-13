b65965941a36b933ee81fc64918f26390dcabe90
Charles Samborski <demurgos@demurgos.net>
2018-08-13 23:07:35 +0200

    Drop Debian-based image

--- 3.1-arch/run-gui.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-# Exit on error
-set -e
-# Echo and run
-CMD="xhost local:docker && docker run -it -e DISPLAY -v $HOME/.Xauthority:/home/root/.Xauthority --net=host demurgos/haxe:3.1-arch" && echo "$CMD" && eval "$CMD"
--- 3.1-arch/run.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-# Exit on error
-set -e
-# Echo and run
-CMD="docker run -it demurgos/haxe:3.1-arch" && echo "$CMD" && eval "$CMD"
